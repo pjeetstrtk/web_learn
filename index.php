f403de0184b7f487ee68ba2e3ffd8cb0c2d5ea28
pjeetstrtk <pjeetstrtk@gmail.com>
2025-05-05 01:47:00 +0600

    last

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-    
-    $servername = "localhost"; // Change if using a remote server
-    $username = "root"; // Your MySQL username
-    $password = ""; // Your MySQL password
-    $database = "web_learn_db"; //The database you want to connect to
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $database);
-    
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
